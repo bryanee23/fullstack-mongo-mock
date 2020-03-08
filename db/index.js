b0e3a3ba5798f0929264fa8686790546c11b9bb2
Hack Reactor Student <student@hackreactor.com>
2020-03-08 12:27:15 -0700

    db is connected and running

--- db/index.js
+++ db/index.js
@@ -1,14 +1,53 @@
-// build your database
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 var productSchema = require('./schema.js');
-var mongoUri = 'mongodb://localhost/ebidProducts';
-// allow use of es6 promises
 mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ebidProducts', {useNewUrlParser: true});
 
-// Connect Mongoose to our local MongoDB via URI specified above and export it below
-var db;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  Product.create({ name: 'small' })
+  console.log("---------------------- DB up")
+});
 
-// Register the productSchema with Mongoose as the 'Product' collection.
-var Product;
+//test if working
+
+productSchema = mongoose.Schema({
+  name: String
+});
+
+
+var Product = mongoose.model('list', productSchema);
+//document name is called product
+
+
+
 
 module.exports = Product;
+
+
+///////////////////////////////////////
+/////// original code save if case stuff breaks
+///////////////////////////////////////
+// // build your database
+// var mongoose = require('mongoose');
+// var productSchema = require('./schema.js');
+// var mongoUri = 'mongodb://localhost/ebidProducts';
+// // allow use of es6 promises
+// mongoose.Promise = global.Promise;
+
+// // Connect Mongoose to our local MongoDB via URI specified above and export it below
+// var db;
+
+// // Register the productSchema with Mongoose as the 'Product' collection.
+// var Product;
+
+
+
+
+
+
+
+
+
+
