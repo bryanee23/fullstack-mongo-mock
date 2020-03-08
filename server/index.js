bf1eb54cfce479f9a3904afbc9dda24117edb8f1
Hack Reactor Student <student@hackreactor.com>
2020-03-08 11:29:19 -0700

    save

--- server/index.js
+++ server/index.js
@@ -4,16 +4,17 @@
 const express = require('express')
 const path = require('path');
 const bodyParser = require('body-parser');
-const bodyParser = require('body-parser');
-
+const morgan = require('morgan');
 const router = require('./router');
 
 const port = 3000;
 const server = express();
 
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use('/', express.static(path.join(__dirname + '/../client/dist')));
 
-server.listen(port, () => console.log('Connected to port: 3000'))
+server.listen(port, () => console.log(`I'll listen to you all day on ${port}`))
 
 
 server.get('/name', (req, res) => {
