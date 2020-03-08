14adc69efb36164e1e072e13b560ae0ce501b67c
Hack Reactor Student <student@hackreactor.com>
2020-03-08 11:24:39 -0700

    fixed nodemon error in run script

--- server/index.js
+++ server/index.js
@@ -1,28 +1,42 @@
-// Express Server
-// FIX ME :(
-  const express = require('express');
-  const path = require('path');
-  const router = require('./router');
+// // Express Server
+// // FIX ME :(
+//   const express = require('express');
+const express = require('express')
+const path = require('path');
+const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-  const port = 3000;
-  const server = express();
+const router = require('./router');
 
-  server.use('/', express.static(path.join(__dirname + '/../client/dist')));
+const port = 3000;
+const server = express();
 
-  server.get('/name', (req, res) => {
-    res.status(200).send('This is your get request, modify this file to use your router!')
-  })
+server.use('/', express.static(path.join(__dirname + '/../client/dist')));
 
-  server.post('/name', (req, res) => {
-    res.status(200).send('This is your post request, modify this file to use your router!')
-  })
+server.listen(port, () => console.log('Connected to port: 3000'))
 
-  server.put('/name', (req, res) => {
-    res.status(200).send('This is your put request, modify this file to use your router!')
-  })
 
-  server.delete('/name', (req, res) => {
-    res.status(200).send('This is your delete request, modify this file to use your router!')
-  })
+server.get('/name', (req, res) => {
+  res.status(200).send('This is your get request, modify this file to use your router!')
+})
 
-  server.listen(port, () => console.log('Connected to port: 3000'))
+server.post('/name', (req, res) => {
+  res.status(200).send('This is your post request, modify this file to use your router!')
+})
+
+server.put('/name', (req, res) => {
+  res.status(200).send('This is your put request, modify this file to use your router!')
+})
+
+server.delete('/name', (req, res) => {
+  res.status(200).send('This is your delete request, modify this file to use your router!')
+})
+
+
+  // const express = require('express')
+  // const app = express()
+  // const port = 3000
+
+  // app.get('/', (req, res) => res.send('Hello World!'))
+
+  // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
